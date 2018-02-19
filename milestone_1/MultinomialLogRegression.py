ef6953e33e7db3b58c387abf146e1fe3d206e262
Jae Sang Ha <jaesangha@Jaes-MacBook-Pro.local>
2018-02-18 22:04:28 -0600

    Dataset modified, MultinomialLogRegression.py modified

--- milestone_1/MultinomialLogRegression.py
+++ milestone_1/MultinomialLogRegression.py
@@ -8,15 +8,21 @@ Created on Sun Feb 11 21:45:18 2018
 
 import pandas as pd
 import numpy as np
+import sys
 from sklearn import linear_model
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 
-DATASET_PATH = "/Users/dohoonkim/Desktop/cse517a/ApplicationProject/winequality-red.csv"
-data_features = ["fa","va","ca","rs","ch","fsd","tsd","dens","pH","sulp","alcohol","eval"]
+def validate_cmdline_args(nargs, msg):
+    if len(sys.argv) < nargs:
+        print(msg)
+        sys.exit(1)
+validate_cmdline_args(2,'Usage: python MultinomialLogRegression.py <DATASET_PATH>')
+#DATASET_PATH = "/Users/dohoonkim/Desktop/cse517a/ApplicationProject/winequality-red.csv"
+DATASET_PATH = sys.argv[1]
+data_features = ["fa","va","ca","rs","ch","fsd","tsd","dens","pH","sulp","alcohol","eval"] #12
 data = pd.read_csv(DATASET_PATH,names=data_features)
 train_x, test_x, train_y, test_y = train_test_split(data[data_features[:10]],data[data_features[11]], train_size=0.7)
-
 
 lr = linear_model.LogisticRegression()
 lr.fit(train_x, train_y)
@@ -24,4 +30,3 @@ mul_lr = linear_model.LogisticRegression(multi_class='multinomial', solver='newt
 
 print('Multinomial Logistic regression Train Accuracy :: {}'.format(metrics.accuracy_score(train_y, mul_lr.predict(train_x))))
 print('Multinomial Logistic regression Test Accuracy :: {}'.format(metrics.accuracy_score(test_y, mul_lr.predict(test_x))))
-
