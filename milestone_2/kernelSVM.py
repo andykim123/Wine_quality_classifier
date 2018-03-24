6567aa09d667aa4464bea2683bd7e7e484279c9c
annielee <annie.lee@wustl.edu>
2018-03-24 18:19:23 -0500

    binary/multiclass

--- milestone_2/kernelSVM.py
+++ milestone_2/kernelSVM.py
@@ -12,7 +12,7 @@ def validate_cmdline_args(nargs, msg):
     if len(sys.argv) < nargs:
         print(msg)
         sys.exit(1)
-validate_cmdline_args(3,'Usage: python decisionTree.py <DATASET_PATH_RED> <DATASET_PATH_WHITE>')
+validate_cmdline_args(3,'Usage: python kernelSVM.py <DATASET_PATH_RED> <DATASET_PATH_WHITE>')
 DATASET_PATH_RED = sys.argv[1]
 DATASET_PATH_WHITE = sys.argv[2]
 
@@ -26,7 +26,11 @@ print('-------------Red Wine Evaluation-------------')
 
 train_x, test_x, train_y, test_y = train_test_split(data_red[data_features[:10]],data_red[data_features[11]], train_size=0.7)
 
+clf_red_fit = clf.fit(train_x,train_y)
 
+print('SVM Multinomial Classification Train Accuracy :: {}'.format(metrics.accuracy_score(train_y, clf_red_fit.predict(train_x))))
+print('SVM Multinomial Classification Test Accuracy :: {}'.format(metrics.accuracy_score(test_y, clf_red_fit.predict(test_x))))
+print('Multinomial CV-prediction error rate :: {}'.format(cross_val_score(clf, data_red[data_features[:10]], data_red[data_features[11]], cv=10)))
 
 mask = (2 < train_y) & (train_y < 5)
 train_y[mask] = 0
@@ -50,26 +54,24 @@ mask = (4 < cv_y) & (cv_y < 7)
 cv_y[mask] = 1
 mask = (6 < cv_y) & (cv_y < 9)
 cv_y[mask] = 2
-#
-#train_y[train_y<5] = 0
-#train_y[train_y>=5] = 1
-#test_y[test_y<5] = 0
-#test_y[test_y>=5] = 1
-#cv_x = data_red[data_features[:10]]
-#cv_y = data_red[data_features[11]]
-#cv_y[cv_y<5] = 0
-#cv_y[cv_y>=5] = 1
 
 clf_red_fit = clf.fit(train_x,train_y)
 
-print('Decision Tree Multinomial Classification Train Accuracy :: {}'.format(metrics.accuracy_score(train_y, clf_red_fit.predict(train_x))))
-print('Decision Tree Multinomial Classification Test Accuracy :: {}'.format(metrics.accuracy_score(test_y, clf_red_fit.predict(test_x))))
-print('Multinomial CV-prediction error rate :: {}'.format(cross_val_score(clf, cv_x, cv_y, cv=10)))
+print('SVM 3-class Classification Train Accuracy :: {}'.format(metrics.accuracy_score(train_y, clf_red_fit.predict(train_x))))
+print('SVM 3-class Classification Test Accuracy :: {}'.format(metrics.accuracy_score(test_y, clf_red_fit.predict(test_x))))
+print('3-class CV-prediction error rate :: {}'.format(cross_val_score(clf, cv_x, cv_y, cv=10)))
+
 
                                     ## White Wine ##
 print('-------------White Wine Evaluation-------------')
 
 train_x_w, test_x_w, train_y_w, test_y_w = train_test_split(data_white[data_features[:10]],data_white[data_features[11]], train_size=0.7)
+
+clf_red_fit = clf.fit(train_x_w,train_y_w)
+
+print('SVM Multinomial Classification Train Accuracy :: {}'.format(metrics.accuracy_score(train_y_w, clf_red_fit.predict(train_x_w))))
+print('SVM Multinomial Classification Test Accuracy :: {}'.format(metrics.accuracy_score(test_y_w, clf_red_fit.predict(test_x_w))))
+print('Multinomial CV-prediction error rate :: {}'.format(cross_val_score(clf, data_red[data_features[:10]], data_red[data_features[11]], cv=10)))
 
 mask = (train_y_w < 5)
 train_y_w[mask] = 0
@@ -94,14 +96,6 @@ cv_y_w[mask] = 1
 mask = (6 < cv_y_w)
 cv_y_w[mask] = 2
 
-#train_y_w[train_y_w<5] = 0
-#train_y_w[train_y_w>=5] = 1
-#test_y_w[test_y_w<5] = 0
-#test_y_w[test_y_w>=5] = 1
-#cv_x_w = data_white[data_features[:10]]
-#cv_y_w = data_white[data_features[11]]
-#cv_y_w[cv_y_w<5] = 0
-#cv_y_w[cv_y_w>=5] = 1
 
 clf_white_fit = clf.fit(train_x_w,train_y_w)
 
