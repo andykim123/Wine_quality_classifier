5bedc02c8751fc3822b3b555cf4732184dce2da6
annielee <annie.lee@wustl.edu>
2018-03-24 16:56:06 -0500

    3 class classification

--- milestone_2/kernelSVM.py
+++ milestone_2/kernelSVM.py
@@ -6,6 +6,8 @@ from sklearn import metrics
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 
+pd.options.mode.chained_assignment = None  # default='warn'
+
 def validate_cmdline_args(nargs, msg):
     if len(sys.argv) < nargs:
         print(msg)
@@ -24,19 +26,41 @@ print('-------------Red Wine Evaluation-------------')
 
 train_x, test_x, train_y, test_y = train_test_split(data_red[data_features[:10]],data_red[data_features[11]], train_size=0.7)
 
-clf_red_fit = clf.fit(train_x,train_y)
 
-train_y[2<train_y] = 0
-train_y[5<=train_y] = 1
-train_y[7<=train_y] = 2
-test_y[2<test_y] = 0
-test_y[5<=test_y] = 1
-test_y[7<=test_y] = 2
+
+mask = (2 < train_y) & (train_y < 5)
+train_y[mask] = 0
+mask = (4 < train_y) & (train_y < 7)
+train_y[mask] = 1
+mask = (6 < train_y) & (train_y < 9)
+train_y[mask] = 2
+
+mask = (2 < test_y) & (test_y < 5)
+test_y[mask] = 0
+mask = (4 < test_y) & (test_y < 7)
+test_y[mask] = 1
+mask = (6 < test_y) & (test_y < 9)
+test_y[mask] = 2
+
 cv_x = data_red[data_features[:10]]
 cv_y = data_red[data_features[11]]
-cv_y[2<cv_y] = 0
-cv_y[5<=cv_y] = 1
-cv_y[7<=cv_y] = 2
+mask = (2 < cv_y) & (cv_y < 5)
+cv_y[mask] = 0
+mask = (4 < cv_y) & (cv_y < 7)
+cv_y[mask] = 1
+mask = (6 < cv_y) & (cv_y < 9)
+cv_y[mask] = 2
+#
+#train_y[train_y<5] = 0
+#train_y[train_y>=5] = 1
+#test_y[test_y<5] = 0
+#test_y[test_y>=5] = 1
+#cv_x = data_red[data_features[:10]]
+#cv_y = data_red[data_features[11]]
+#cv_y[cv_y<5] = 0
+#cv_y[cv_y>=5] = 1
+
+clf_red_fit = clf.fit(train_x,train_y)
 
 print('Decision Tree Multinomial Classification Train Accuracy :: {}'.format(metrics.accuracy_score(train_y, clf_red_fit.predict(train_x))))
 print('Decision Tree Multinomial Classification Test Accuracy :: {}'.format(metrics.accuracy_score(test_y, clf_red_fit.predict(test_x))))
@@ -47,20 +71,40 @@ print('-------------White Wine Evaluation-------------')
 
 train_x_w, test_x_w, train_y_w, test_y_w = train_test_split(data_white[data_features[:10]],data_white[data_features[11]], train_size=0.7)
 
-clf_white_fit = clf.fit(train_x_w,train_y_w)
+mask = (train_y_w < 5)
+train_y_w[mask] = 0
+mask = (4 < train_y_w) & (train_y_w < 7)
+train_y_w[mask] = 1
+mask = (6 < train_y_w)
+train_y_w[mask] = 2
 
-train_w_y[2<train_w_y] = 0
-train_w_y[5<=train_w_y] = 1
-train_w_y[7<=train_w_y] = 2
-test_w_y[2<test_w_y] = 0
-test_w_y[5<=test_w_y] = 1
-test_w_y[7<=test_w_y] = 2
-cv_w_x = data_white[data_features[:10]]
-cv_w_y = data_white[data_features[11]]
-cv_w_y[2<cv_w_y] = 0
-cv_w_y[5<=cv_w_y] = 1
-cv_w_y[7<=cv_w_y] = 2
+mask = (test_y_w < 5)
+test_y_w[mask] = 0
+mask = (4 < test_y_w) & (test_y_w < 7)
+test_y_w[mask] = 1
+mask = (6 < test_y_w)
+test_y_w[mask] = 2
+
+cv_x_w = data_white[data_features[:10]]
+cv_y_w = data_white[data_features[11]]
+mask = (cv_y_w < 5)
+cv_y_w[mask] = 0
+mask = (4 < cv_y_w) & (cv_y_w < 7)
+cv_y_w[mask] = 1
+mask = (6 < cv_y_w)
+cv_y_w[mask] = 2
+
+#train_y_w[train_y_w<5] = 0
+#train_y_w[train_y_w>=5] = 1
+#test_y_w[test_y_w<5] = 0
+#test_y_w[test_y_w>=5] = 1
+#cv_x_w = data_white[data_features[:10]]
+#cv_y_w = data_white[data_features[11]]
+#cv_y_w[cv_y_w<5] = 0
+#cv_y_w[cv_y_w>=5] = 1
+
+clf_white_fit = clf.fit(train_x_w,train_y_w)
 
 print('Decision Tree Multinomial Classification Train Accuracy :: {}'.format(metrics.accuracy_score(train_y_w, clf_white_fit.predict(train_x_w))))
 print('Decision Tree Multinomial Classification Test Accuracy :: {}'.format(metrics.accuracy_score(test_y_w, clf_white_fit.predict(test_x_w))))
-print('Multinomial CV-prediction error rate :: {}'.format(cross_val_score(clf, cv_w_x, cv_w_y, cv=10)))
+print('Multinomial CV-prediction error rate :: {}'.format(cross_val_score(clf, cv_x_w, cv_y_w, cv=10)))
