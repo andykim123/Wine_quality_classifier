554dfb595332182139caf083b2ebb7aa7bb5a926
Ryun Han <r.han@wustl.edu>
2018-03-25 22:43:09 -0500

    model evaluation literally almost done

--- milestone_2/modelEvaluation.py
+++ milestone_2/modelEvaluation.py
@@ -35,7 +35,7 @@ model_2 = sys.argv[2]
 list_1 = []
 list_2 = []
 
-list_2 = random.sample(xrange(100), 10)
+# list_2 = random.sample(xrange(100), 10)
 
 print("Files to be used:")
 print("Model 1: "+model_1)
@@ -43,27 +43,30 @@ print("Model 2: "+model_2)
 print("-----------------------")
 print("Model Calculation Starts")
 for k in range(0,10):
-    print(str(k)+"th Run:")
+    print("    "+str(k+1)+"th Run:")
     # proc = subprocess.Popen(['python', model_1,  DATASET_PATH, "true"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     # print(proc.communicate()[0])
+    print("        "+model_1+":")
     proc1 = subprocess.check_output([sys.executable, model_1, DATASET_PATH, "true"])
-    # proc2 = subprocess.check_output([sys.executable, model_2, DATASET_PATH, "true"])
+    print("        "+model_2+":")
+    proc2 = subprocess.check_output([sys.executable, model_2, DATASET_PATH, "true"])
     temp_1 = proc1.split("[")[1].split("]")[0].split()
-    # temp_2 = proc2.split("[")[1].split("]")[0].split()
+    temp_2 = proc2.split("[")[1].split("]")[0].split()
     for x in range(0,len(temp_1)):
 	    list_1.append(float(temp_1[x]))
-	    # list_2.append(float(temp_2[x]))
+	    list_2.append(float(temp_2[x]))
 
 print("Model Calculation Ends")
+print("-----------------------")
 # print(model_1+"- Mean: "+str(np.mean(list_1))+", Standard Deviation: "+str(np.sqrt(np.var(list_1))))
 t_test_result = stats.ttest_ind(list_1,list_2,equal_var=False)
-print(t_test_result.pvalue)
+print("P-Value: "+str(t_test_result.pvalue))
 if t_test_result.statistic<0 and t_test_result.pvalue<0.05:
     print("Significantly, "+model_2+" is better than "+model_1)
 elif t_test_result.statistic>=0 and t_test_result.pvalue<0.05:
 	print("Significantly, "+model_1+" is better than "+model_2)
 else:
     if t_test_result.static>=0:
-        print("Statistically, no difference detected. But in this sample, "+model_1+"is slightly better")
+        print("Statistically, no difference detected. But in this sample, "+model_1+"is slightly better.\n"+model_1+" mean accuracy: "+str(np.mean(list_1)))
     else:
-        print("Statistically, no difference detected. But in this sample, "+model_2+"is slightly better")
+        print("Statistically, no difference detected. But in this sample, "+model_2+"is slightly better.\n"+model_2+" mean accuracy: "+str(np.mean(list_2)))
