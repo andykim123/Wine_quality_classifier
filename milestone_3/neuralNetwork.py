598f3090d78d6c82c461b0ec85697a162b894ab9
annielee <annie.lee@wustl.edu>
2018-04-08 17:04:13 -0500

    initial

--- milestone_3/neuralNetwork.py
+++ milestone_3/neuralNetwork.py
@@ -6,4 +6,19 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 
+def validate_cmdline_args(nargs, msg):
+    if len(sys.argv) < nargs:
+        print(msg)
+        sys.exit(1)
+validate_cmdline_args(3,'Usage: python neuralNetwork.py <DATASET_PATH_RED> <DATASET_PATH_WHITE>')
+DATASET_PATH_RED = sys.argv[1]
+DATASET_PATH_WHITE = sys.argv[2]
+
+data_features = ["fa","va","ca","rs","ch","fsd","tsd","dens","pH","sulp","alcohol","eval"]
+data_red = pd.read_csv(DATASET_PATH_RED,names=data_features)
+data_white = pd.read_csv(DATASET_PATH_RED,names=data_features)
+
+                            ## Red Wine ##
+print('-------------Red Wine Evaluation-------------')
+
 
