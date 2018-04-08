64dae7ccc448017e0026315ef900a410e4557608
Jae Sang Ha <jaesangha@jaes-mbp.dhcp.wustl.edu>
2018-04-08 17:12:36 -0500

    a

--- milestone_3/neuralNetwork.py
+++ milestone_3/neuralNetwork.py
@@ -5,6 +5,8 @@ import sys
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
 
 def validate_cmdline_args(nargs, msg):
     if len(sys.argv) < nargs:
@@ -17,8 +19,17 @@ DATASET_PATH_WHITE = sys.argv[2]
 data_features = ["fa","va","ca","rs","ch","fsd","tsd","dens","pH","sulp","alcohol","eval"]
 data_red = pd.read_csv(DATASET_PATH_RED,names=data_features)
 data_white = pd.read_csv(DATASET_PATH_RED,names=data_features)
-
-                            ## Red Wine ##
+model = tf.estimator.LinearRegressor(feature_columns=data_features)
+train_x, test_x, train_y, test_y = train_test_split(data_red[data_features[:10]],data_red[data_features[11]], train_size=0.7)
+def input_train():
+    return (
+        # Shuffling with a buffer larger than the data set ensures
+        # that the examples are well mixed.
+        train_x.shuffle(1000).batch(128)
+        # Repeat forever
+        .repeat().make_one_shot_iterator().get_next())
+model.train(input_fn=input_train, steps=STEPS)
+# model.train(train_x, 1000)
+eval_result = model.evaluate(test)
+                ## Red Wine ##
 print('-------------Red Wine Evaluation-------------')
-
-
