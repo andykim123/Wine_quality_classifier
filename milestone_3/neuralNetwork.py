058e8b86035b90111d3f8635d727eba556c69af0
annielee <annie.lee@wustl.edu>
2018-04-10 13:26:06 -0500

    merging

--- milestone_3/neuralNetwork.py
+++ milestone_3/neuralNetwork.py
@@ -29,9 +29,18 @@ data_white = pd.read_csv(DATASET_PATH_RED,names=data_features)
                             ## Red Wine ##
 print('-------------Red Wine Evaluation-------------')
 
+np.random.seed(None)
+
 x_train = data_red.sample(frac=0.7, random_state=None)
 x_test = data_red.drop(x_train.index)
+y_train = x_train.pop("eval")
+y_test = x_test.pop("eval")
 
-print(x_train)
-print(x_test)
+train = (x_train, y_train)
+test = (x_test, y_test)
+
+train.shuffle(1000).batch(128).repeat().make_one_shot_iterator().get_next()
+
+print(train)
+print(test)
 
